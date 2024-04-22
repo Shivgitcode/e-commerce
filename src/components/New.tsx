adc8b97e81d4cf04ccc51bf9bd1976d0c3153f37
shivansh <shivneeraj2004@gmail.com>
2024-04-22 16:40:11 +0530

    made the main section

--- src/components/New.tsx
+++ src/components/New.tsx
@@ -23,13 +23,13 @@ export default function New() {
                 </div>
 
             </section>
-            <section className="flex items-center w-full justify-between gap-5">
-                <div className="flex flex-col gap-5 ">
-                    <div className="relative w-[486px] p-[10px] h-[180px] bg-[#bce7f0] rounded-[10px] flex justify-center items-center group overflow-hidden">
+            <section className="flex items-center w-full justify-between gap-5 mb-5">
+                <div className="flex flex-col gap-5 flex-1">
+                    <div className="relative w-full p-[10px] h-[180px] bg-[#bce7f0] rounded-[10px] flex justify-center items-center group overflow-hidden">
                         <Image src={"/asset 1.png"} alt="no" fill className=" object-cover group-hover:scale-110 transition-all duration-500"></Image>
                         <p className=" text-3xl font-bold z-[1] relative group-hover:scale-110 transition-all duration-500">Get up to 50% off</p>
                     </div>
-                    <div className="relative w-[486px] h-[180px] rounded-[10px] bg-[#dec8f3] p-[10px] flex justify-start items-center group overflow-hidden">
+                    <div className="relative w-full h-[180px] rounded-[10px] bg-[#dec8f3] p-[10px] flex justify-start items-center group overflow-hidden">
                         <div className="relative z-[1] pl-10 mr-[11px] group-hover:scale-110 transition-all duration-500 ease-in-out">
                             <p className="text-[18.72px] font-bold mb-[18px]">New Jordan Series</p>
                             <p className="text-[14px] font-medium">Best of daily wear</p>
@@ -40,8 +40,8 @@ export default function New() {
                     </div>
                 </div>
 
-                <div className="flex w-full gap-[20px]">
-                    <div className="flex flex-col items-center p-[10px] w-[233px] h-[400px] bg-[#f6f6f6] rounded-xl justify-around group">
+                <div className="flex w-full gap-[20px] flex-1">
+                    <div className="flex flex-1 flex-col items-center p-[10px] w-[233px] h-[400px] bg-[#f6f6f6] rounded-xl justify-around group">
                         <div className="relative w-[213px] h-[245px] group-hover:scale-110 transition-all duration-500">
                             <Image src={"/asset 3.png"} alt="no" fill objectFit="cover" objectPosition="top"></Image>
                         </div>
@@ -55,7 +55,7 @@ export default function New() {
                             </p>
                         </div>
                     </div>
-                    <div className="flex flex-col items-center p-[10px] w-[233px] h-[400px] bg-[#f6f6f6] rounded-xl justify-around group">
+                    <div className="flex flex-col flex-1 items-center p-[10px] w-[233px] h-[400px] bg-[#f6f6f6] rounded-xl justify-around group">
                         <div className="relative w-[213px] h-[245px] group-hover:scale-110 transition-all duration-500">
                             <Image src={"/asset 4.png"} alt="no" fill objectFit="cover" objectPosition="top"></Image>
                         </div>
@@ -70,6 +70,101 @@ export default function New() {
 
 
             </section>
+            <section className="flex items-center w-full justify-between gap-5 mb-5 flex-row-reverse">
+                <div className="flex flex-col gap-5 flex-1">
+                    <div className="relative w-full p-[10px] h-[180px] bg-[#bce7f0] rounded-[10px] flex justify-center items-center group overflow-hidden">
+                        <Image src={"/asset 1.png"} alt="no" fill className=" object-cover group-hover:scale-110 transition-all duration-500"></Image>
+                        <p className=" text-3xl font-bold z-[1] relative group-hover:scale-110 transition-all duration-500">Get up to 50% off</p>
+                    </div>
+                    <div className="relative w-full h-[180px] rounded-[10px] bg-[#dec8f3] p-[10px] flex justify-start items-center group overflow-hidden">
+                        <div className="relative z-[1] pl-10 mr-[11px] group-hover:scale-110 transition-all duration-500 ease-in-out">
+                            <p className="text-[18.72px] font-bold mb-[18px]">New Jordan Series</p>
+                            <p className="text-[14px] font-medium">Best of daily wear</p>
+
+                        </div>
+
+                        <Image src={"/asset 2.png"} alt="no" fill objectFit="contain" objectPosition="right" className=" group-hover:scale-110 transition-all duration-500 ease-in-out"></Image>
+                    </div>
+                </div>
+
+                <div className="flex w-full gap-[20px] flex-1">
+                    <div className="flex flex-1 flex-col items-center p-[10px] w-[233px] h-[400px] bg-[#f6f6f6] rounded-xl justify-around group">
+                        <div className="relative w-[213px] h-[245px] group-hover:scale-110 transition-all duration-500">
+                            <Image src={"/asset 8.png"} alt="no" fill objectFit="cover" objectPosition="top"></Image>
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <h3 className=" text-sm font-bold mb-4 ">Hugo Boss Leather Jacket</h3>
+                            <p className="text-[20px] font-bold">
+                                <div className=" line-through text-[#808080] text-[14px]">
+                                    300$
+                                </div>
+                                200$
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col flex-1 items-center p-[10px] w-[233px] h-[400px] bg-[#f6f6f6] rounded-xl justify-around group">
+                        <div className="relative w-[213px] h-[245px] group-hover:scale-110 transition-all duration-500">
+                            <Image src={"/asset 7.png"} alt="no" fill objectFit="cover" objectPosition="top"></Image>
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <h3 className=" text-sm font-bold mb-4 ">Polka-dotted slip dress</h3>
+                            <p className="text-[20px] font-bold">
+                                200$
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </section>
+            <section className="flex items-center w-full justify-between gap-5 mb-5">
+                <div className="flex flex-col gap-5 flex-1">
+                    <div className="relative w-full p-[10px] h-[180px] bg-[#bce7f0] rounded-[10px] flex justify-center items-center group overflow-hidden">
+                        <Image src={"/asset 1.png"} alt="no" fill className=" object-cover group-hover:scale-110 transition-all duration-500"></Image>
+                        <p className=" text-3xl font-bold z-[1] relative group-hover:scale-110 transition-all duration-500">Get up to 50% off</p>
+                    </div>
+                    <div className="relative w-full h-[180px] rounded-[10px] bg-[#dec8f3] p-[10px] flex justify-start items-center group overflow-hidden">
+                        <div className="relative z-[1] pl-10 mr-[11px] group-hover:scale-110 transition-all duration-500 ease-in-out">
+                            <p className="text-[18.72px] font-bold mb-[18px]">New Jordan Series</p>
+                            <p className="text-[14px] font-medium">Best of daily wear</p>
+
+                        </div>
+
+                        <Image src={"/asset 2.png"} alt="no" fill objectFit="contain" objectPosition="right" className=" group-hover:scale-110 transition-all duration-500 ease-in-out"></Image>
+                    </div>
+                </div>
+
+                <div className="flex w-full gap-[20px] flex-1">
+                    <div className="flex flex-1 flex-col items-center p-[10px] w-[233px] h-[400px] bg-[#f6f6f6] rounded-xl justify-around group">
+                        <div className="relative w-[213px] h-[245px] group-hover:scale-110 transition-all duration-500">
+                            <Image src={"/asset 3.png"} alt="no" fill objectFit="cover" objectPosition="top"></Image>
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <h3 className=" text-sm font-bold mb-4 ">Hugo Boss Leather Jacket</h3>
+                            <p className="text-[20px] font-bold">
+                                <div className=" line-through text-[#808080] text-[14px]">
+                                    300$
+                                </div>
+                                200$
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col flex-1 items-center p-[10px] w-[233px] h-[400px] bg-[#f6f6f6] rounded-xl justify-around group">
+                        <div className="relative w-[213px] h-[245px] group-hover:scale-110 transition-all duration-500">
+                            <Image src={"/asset 4.png"} alt="no" fill objectFit="cover" objectPosition="top"></Image>
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <h3 className=" text-sm font-bold mb-4 ">Polka-dotted slip dress</h3>
+                            <p className="text-[20px] font-bold">
+                                200$
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </section>
+
         </div>
     )
 }
